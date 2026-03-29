8425cb20074e4e24fde71be26d54475716a41af0
Magni Mar Magnason <magni@optitog.com>
2026-03-29 23:04:33 +0000

    feat: add logo to top left corner linking to home

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -6,4 +6,11 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<div class="absolute top-4 left-4 z-50">
+	<a href="/" class="block hover:opacity-80 transition-opacity">
+		<img src="/logo/logo.png" alt="Properties by Magni Logo" class="h-12 w-auto" />
+	</a>
+</div>
+
 {@render children()}
