1aed8b4816ef7f8e25eeef8fa0481c9d6857b9d6
Magni Mar Magnason <magni@optitog.com>
2026-04-02 23:29:36 +0000

    Remove test_map.py from repository

--- test_map.py
+++
@@ -1,7 +0,0 @@
-import urllib.parse
-
-api_key = "AIzaSyAAJL11FGR1AImjuxi9kYcxmBTovEZqS7s"
-map_addr = "Fasteign, Iceland"
-map_addr_encoded = urllib.parse.quote(map_addr)
-static_map_url = f"https://maps.googleapis.com/maps/api/staticmap?center={map_addr_encoded}&zoom=15&size=600x200&scale=2&maptype=roadmap&markers=color:red%7C{map_addr_encoded}&key={api_key}"
-print(static_map_url)
