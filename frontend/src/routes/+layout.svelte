c8400d0265026ba4ae9bfbca757df5d4e3dc7e20
Magni Mar Magnason <magni@optitog.com>
2026-04-02 23:01:35 +0000

    finido

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,16 +1,10 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+        import './layout.css';
+        import favicon from '$lib/assets/favicon.svg';
 
-	let { children } = $props();
+        let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-
-<div class="absolute top-4 left-4 z-50">
-	<a href="/home" class="block hover:opacity-80 transition-opacity">
-		<img src="/logo/logo.png" alt="Properties by Magni Logo" class="h-24 w-auto" />
-	</a>
-</div>
 
 {@render children()}
