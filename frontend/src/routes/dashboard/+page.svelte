1254b40d7d72f1279266b60d9d1b3698e9a67bb8
Magni Mar Magnason <magnimar@live.com>
2026-04-07 15:11:57 +0000

    feat: add active subscription banner for pro users

--- frontend/src/routes/dashboard/+page.svelte
+++ frontend/src/routes/dashboard/+page.svelte
@@ -640,21 +640,38 @@
         </button>
     </div>
     
-    {#if user && !user.is_pro}
-        <div class="bg-yellow-50 border-l-4 border-yellow-400 p-4 mb-8 mt-12 rounded-md shadow-sm">
-            <div class="flex">
-                <div class="flex-shrink-0">
-                    <svg class="h-5 w-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
-                    </svg>
-                </div>
-                <div class="ml-3">
-                    <p class="text-sm text-yellow-700 font-medium">
-                        Þú ert á prufureikning, til þess að hafa fullt aðgengi að öllum eiginleikum síðunnar verður þú að kaupa áskrift.
-                    </p>
+    {#if user}
+        {#if !user.is_pro}
+            <div class="bg-yellow-50 border-l-4 border-yellow-400 p-4 mb-8 mt-12 rounded-md shadow-sm">
+                <div class="flex">
+                    <div class="flex-shrink-0">
+                        <svg class="h-5 w-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                    <div class="ml-3">
+                        <p class="text-sm text-yellow-700 font-medium">
+                            Þú ert á prufureikning, til þess að hafa fullt aðgengi að öllum eiginleikum síðunnar verður þú að kaupa áskrift.
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
+        {:else}
+            <div class="bg-green-50 border-l-4 border-green-400 p-4 mb-8 mt-12 rounded-md shadow-sm">
+                <div class="flex">
+                    <div class="flex-shrink-0">
+                        <svg class="h-5 w-5 text-green-400" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                    <div class="ml-3">
+                        <p class="text-sm text-green-700 font-medium">
+                            Þú ert með virka áskrift og hefur aðgengi að öllum eiginleikum síðunnar.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        {/if}
     {/if}
 
     <div class="flex justify-center items-center mb-8 text-center mt-6">
